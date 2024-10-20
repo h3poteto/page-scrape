90f8d97b3e728bcafe5e7d9dffc54390054be9ea
AkiraFukushima <h3.poteto@gmail.com>
2024-10-20 22:14:16 +0900

    Post discord

--- main.rb
+++ main.rb
@@ -1,11 +1,20 @@
 require "open-uri"
 require "nokogiri"
 require "pry"
+require "net/http"
 
-page = Nokogiri.HTML(URI.open("https://herlipto.jp/products/1243305090"))
+url = "https://herlipto.jp/products/1243305090"
+page = Nokogiri.HTML(URI.open(url))
+webhook = ENV["DISCORD_WEBHOOK"]
 
 if page.xpath("//product-meta/div[1]/div[2]/span[1]").text == "Sold out"
   puts "Sold out"
 else
   puts "Now on sale"
+  Net::HTTP.post_form(
+    URI.parse(webhook),
+    {
+      username: "PageWatcher",
+      content: "入荷したかも？ #{url}"
+    })
 end
