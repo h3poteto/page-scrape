bbed5d4672d504ded64fee129adb1edab36f40bb
AkiraFukushima <h3.poteto@gmail.com>
2024-10-20 22:20:02 +0900

    Fix target

--- main.rb
+++ main.rb
@@ -7,7 +7,9 @@ url = "https://herlipto.jp/products/1243305090"
 page = Nokogiri.HTML(URI.open(url))
 webhook = ENV["DISCORD_WEBHOOK"]
 
-if page.xpath("//product-meta/div[1]/div[2]/span[1]").text == "Sold out"
+target = page.xpath("//product-meta/div[1]/div[2]/span[1]")
+
+if target && target.text == "Sold out"
   puts "Sold out"
 else
   puts "Now on sale"
